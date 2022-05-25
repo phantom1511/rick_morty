39b13ebdd5b96ea7bf45b431641755b3b6efb236
phantom1511 <44024559+phantom1511@users.noreply.github.com>
2022-05-25 18:45:28 +0600

    added title for main screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: const Text('RICK AND MORTY'),
       ),
       body: Container(),
       bottomNavigationBar:
